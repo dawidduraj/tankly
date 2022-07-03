ba9c5413da5b4e62b3cbf37af52b06f3f658f324
Dawid Duraj <dawid.duraj@outlook.de>
2022-07-03 16:17:07 +0200

    Add search route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
 
 #config
 app = Flask(__name__)
@@ -7,6 +7,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/search",methods = ['POST', 'GET'])
+def search():
+    if request.method == "GET":
+        return redirect("/")
+    return request.form.get("address")
 
 if __name__ == "__main__":
     app.run(debug=True)
